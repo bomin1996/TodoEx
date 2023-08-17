279f83088f49aefc8f48d14d8691b647bf8d95aa
bomin <bomin199628@gmail.com>
2023-08-17 16:42:52 +0900

    style: token access
    token access
    
    Closes #1

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,13 +1,11 @@
 import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { AuthService } from './auth.service';
 import { UserService } from '../user/user.service';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(
-    private readonly authService: AuthService,
     private readonly userService: UserService,
   ) {
     super({ usernameField: 'email', passwordField: 'password' });
